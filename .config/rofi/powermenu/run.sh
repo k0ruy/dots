e0d1f6acf079a6e0ff6a88f33e01d8c198242112
Christian Berchtold <chri.berchtold@gmail.com>
2024-08-10 17:13:28 +0200

    updates

--- .config/rofi/powermenu/run.sh
+++ .config/rofi/powermenu/run.sh
@@ -90,11 +90,7 @@ case ${chosen} in
 		run_cmd --reboot
         ;;
     $lock)
-		if [[ -x '/usr/bin/betterlockscreen' ]]; then
-			betterlockscreen -l
-		elif [[ -x '/usr/bin/i3lock' ]]; then
-			i3lock
-		fi
+    dm-tool lock
         ;;
     $suspend)
 		run_cmd --suspend
